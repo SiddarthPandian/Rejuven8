39b02950ebbe6b9c4a6936494f0f222732b51841
Siddarth <siddarth.pandian@gmail.com>
2020-01-25 16:13:07 -0600

    First commit

---
+++ src/components/navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="https://www.aa.com/homePage.do">
+          American Airlines
+          <span className="badge badge-pill badge-secondary m-2">
+            {this.props.totalCounters}
+          </span>
+        </a>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
