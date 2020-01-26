0053442c84ae64c11ee9692aefc6f54b0807066b
Siddarth <siddarth.pandian@gmail.com>
2020-01-25 19:23:13 -0600

    Override stuff

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from "react";
-import NavBar from "./components/navbar";
-import logo from "./logo.svg";
+import React, { Component } from "react";
+import InputForm from "./components/Note/InputForm";
+//import { DB_CONFIG } from "./Config/config";
+import "firebase/database";
 import "./App.css";
 
-function App() {
-  return (
-    <React.Fragment>
-      <NavBar /
-    </React.Fragment>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: []
+    };
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="notesWrapper">
+          <div className="notesHeader">American Airlines</div>
+          <InputForm />
+          <div className="notesBody"></div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Note/InputForm.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./InputForm.css";
+
+class InputForm extends Component {
+  render() {
+    return <div className="txtFormat">Hello Does this work??</div>;
+  }
+}
+
+export default InputForm;
